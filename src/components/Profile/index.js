3d86a63123b1255bb9b7c3922d04af75367cb2c3
Marcello <marcellorocha632@gmail.com>
2022-10-20 11:15:45 -0300

    Styling and Setting up the Profile component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './style.css';
 
 const Profile = ({ data }) => {
     return (
         <>
             <div className='profile-container'>
-                <img src={data?.avatar_url} alt="User avatar" />
-                <h2>{data?.name}</h2>
-                <h3>{data?.login}</h3>
-                <p>{data?.bio}</p>
+                <img className="user-avatar" src={data?.avatar_url} alt="User avatar" />
+                <a href={`https://github.com/${data?.login}`} className='user-name'><h1>{data?.name}</h1></a>
+                <h3 className='user-login'>{data?.login}</h3>
+                <p className='user-bio' >{data?.bio}</p>
+                <div className='user-stats'>
+                    <a className='stat'>{data?.followers} followers</a>
+                    <a className='stat'>{data?.following} following</a>
+                </div>
+
+                <p className='user-location'>{data?.location}</p>
             </div>
         </>
     );
