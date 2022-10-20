e0f74e5487e15f4d1a93d437017675c646d0cd93
Marcello <marcellorocha632@gmail.com>
2022-10-20 09:14:15 -0300

    Implemmenting the user search function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/index.js';
 import './App.css';
 
 function App() {
+
+  const [ searchValue, setSearchValue ] = useState('');
+
+  async function searchUser() {
+    await fetch(`https://api.github.com/users/${searchValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+  }
+
   return (
     <>
-      <Header />
+      <Header searchValue={searchValue} onChange={(e) => setSearchValue(e.target.value)} onClick={searchUser} />
     </>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './style.css';
 
-const Header = () => {
+const Header = ({ searchText, onChange, onClick }) => {
     return (
         <>
             <header>
                 <h2>Github User Searcher</h2>
-                <input className="input-search" type='text' placeholder='Type a User Here...' />
-                <button className="btn-search">Search</button>
+                <input className="input-search" value={searchText} onChange={onChange} type='text' placeholder='Type a User Here...' />
+                <button className="btn-search" onClick={onClick}>Search</button>
             </header>
         </>
     );
