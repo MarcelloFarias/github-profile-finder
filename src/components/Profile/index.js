a90cf3a765d7a58cd9807f31e5a857b9f9846392
Marcello <marcellorocha632@gmail.com>
2022-10-20 10:37:40 -0300

    Structure The Profile component

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -4,7 +4,10 @@ const Profile = ({ data }) => {
     return (
         <>
             <div className='profile-container'>
-                
+                <img src={data?.avatar_url} alt="User avatar" />
+                <h2>{data?.name}</h2>
+                <h3>{data?.login}</h3>
+                <p>{data?.bio}</p>
             </div>
         </>
     );
