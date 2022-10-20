e1e04f7103fdd0ea60ecaab253b2794d82503bb9
Marcello <marcellorocha632@gmail.com>
2022-10-20 11:24:42 -0300

    Changing the Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,9 +5,11 @@ const Header = ({ searchText, onChange, onClick }) => {
     return (
         <>
             <header>
-                <h2>Github User Searcher</h2>
-                <input className="input-search" value={searchText} onChange={onChange} type='text' placeholder='Type a User Here...' />
-                <button className="btn-search" onClick={onClick}>Search</button>
+                <h1>Github User Searcher</h1>
+                <div className="input-field">
+                    <input className="input-search" value={searchText} onChange={onChange} type='text' placeholder='Type a User Here...' />
+                    <button className="btn-search" onClick={onClick}>Search</button>
+                </div>
             </header>
         </>
     );
