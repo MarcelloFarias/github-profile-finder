a71ca0fbe314342af5b228aadbc50f059244c0b3
Marcello <marcellorocha632@gmail.com>
2022-10-20 13:53:52 -0300

    Correcting placeholder orthography

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ const Header = ({ searchText, onChange, onClick }) => {
             <header>
                 <h1>Github User Searcher</h1>
                 <div className="input-field">
-                    <input className="input-search" value={searchText} onChange={onChange} type='text' placeholder='Type a User Here...' />
+                    <input className="input-search" value={searchText} onChange={onChange} type='text' placeholder='Type an User Here...' />
                     <button className="btn-search" onClick={onClick}>Search</button>
                 </div>
             </header>
