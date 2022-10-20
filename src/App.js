083a7c4697bb75f38217817d71cfbe4c645e02a8
Marcello <marcellorocha632@gmail.com>
2022-10-20 09:37:33 -0300

    Implemmenting the Profile component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import Header from './components/Header/index.js';
+import Profile from './components/Profile/index.js';
 import './App.css';
 
 function App() {
 
   const [ searchValue, setSearchValue ] = useState('');
+  const [ userData, setUserData ] = useState();
 
   async function searchUser() {
     await fetch(`https://api.github.com/users/${searchValue}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      setUserData(data);
     })
     .catch((error) => console.log(error));
   }
@@ -18,6 +20,9 @@ function App() {
   return (
     <>
       <Header searchValue={searchValue} onChange={(e) => setSearchValue(e.target.value)} onClick={searchUser} />
+      <main>
+        <Profile data={userData} />
+      </main>
     </>
   );
 }
---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = ({ data }) => {
+    return (
+        <>
+            <div className='profile-container'>
+                
+            </div>
+        </>
+    );
+}
+
+export default Profile;
